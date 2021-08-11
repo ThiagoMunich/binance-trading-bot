388e99374666c76d9ed41c042a10ee0546469322
thiagomunich <thiagomunich@gmail.com>
2021-08-11 00:36:32 -0300

    manutenção

--- Binance.py
+++ Binance.py
@@ -9,10 +9,11 @@ from binance.enums import *
 from binance.client import Client, requests
 # import config
 
-SOCKET = "wss://fstream.binance.com/ws/btcusdt@kline_15m"
-# SOCKET = "wss://stream.binance.com:9443/ws/btcusdt@kline_1m"
+# SOCKET = "wss://fstream.binance.com/ws/btcusdt@kline_1m"
+SOCKET = "wss://stream.binance.com:9443/ws/btcusdt@kline_1m"
 
-cliente = Client('o1Q5E6C5rpqYBm5NcXimI75Y3nbcim9wXq3sH3O76TSg6vrTwBSRMx1yCGOiHdOo', 'Hlpmd3TlY0H8LsPD4PZOaO5CUZnibIONTHFF3cO12ADSLz5FDRbXRZyM41ArRHCw')
+cliente = Client('o1Q5E6C5rpqYBm5NcXimI75Y3nbcim9wXq3sH3O76TSg6vrTwBSRMx1yCGOiHdOo',
+                 'Hlpmd3TlY0H8LsPD4PZOaO5CUZnibIONTHFF3cO12ADSLz5FDRbXRZyM41ArRHCw')
 
 operacoesAbertas = []
 
@@ -34,9 +35,9 @@ def onMessage(ws, mensagem):
 
     if candleFechado:
 
-        #horarioNegociacao = pd.to_datetime(candle['T'], unit='ms')
+        horario = pd.to_datetime(candle['T'], unit='ms')
 
-        obterSinal(minima=candle['l'], fechamento=candle['c'])
+        obterSinal(minima=candle['l'], fechamento=candle['c'], horario=horario)
 
     else:
 
@@ -98,38 +99,67 @@ def binanceDataFrame(self, klines):
     return df
 
 
+def condicaoCompra(fechamento, ma21Fechamento, ma3Minimas, centavos):
+    if(fechamento < ma3Minimas[-1]):
+        return True
+    else:
+        pass
+
+
 cesta = ['BTCUSDT']
 
 
-def obterSinal(minima, fechamento):
+def obterSinal(minima, fechamento, horario):
 
     for ativoCesta in cesta:
 
         dados = np.array(cliente.futures_klines(
-            symbol=ativoCesta, interval=KLINE_INTERVAL_15MINUTE))
+            symbol=ativoCesta, interval=KLINE_INTERVAL_1MINUTE))
+
         df = binanceDataFrame(dados, dados)
+
         df.set_index('Open Time', inplace=True)
-        df = df[['Open', 'High', 'Low', 'Close', 'Volume']]
-        open, high, low, close = df['Open'], df['High'], df['Low'], df['Close']
 
-        rsi = ta.momentum.rsi(close, 2)
+        df = df[['High', 'Low', 'Close']]
 
-        mediaMovel = ta.trend.sma_indicator(close, 5)
+        high, low, close = df['High'], df['Low'], df['Close']
 
-        precoLimit = float(fechamento) - 5.00
+        mediaMovelVinteUmFechamentos = ta.trend.sma_indicator(close, 21)
+
+        mediaMovelTresUltimasMinimas = ta.trend.sma_indicator(low, 3)
+
+        mediaMovelTresUltimasMaximas = ta.trend.sma_indicator(high, 3)
+
+        precoLimit = float(fechamento) - 1.00
 
         centavos = minima.split('.')
 
-        print('Fechamento: {} | Centavos {}'.format(
-            fechamento, float(centavos[1])))
+        file = open("operacoes.txt", "a")
+        file.write("Horário entrada: %s" % horario)
+        file.close()
+
+        print('Fechamento: {} | Centavos {} | Horário: {}'.format(
+            fechamento, float(centavos[1]), horario))
 
         if len(operacoesAbertas) == 0:
-            if rsi[-1] <= 5.0 and float(centavos[1]) == 0.0:
-                abrirPosicao(ativo=ativoCesta, lote=0.5,
-                             lado=SIDE_BUY, preco=precoLimit)
+            if condicaoCompra(fechamento=float(fechamento), ma21Fechamento=mediaMovelVinteUmFechamentos, ma3Minimas=mediaMovelTresUltimasMinimas, centavos=float(centavos[1])):
+                # abrirPosicao(ativo=ativoCesta, lote=0.5,
+                #              lado=SIDE_BUY, preco=precoLimit)
+
+                # remover quando for operar em conta real
+                operacoesAbertas.append(ativoCesta)
+
+                file = open("operacoes.txt", "a")
+
+                file.write("Horário entrada: %d" % 1)
+
                 print('----------------------------------')
                 print("COMPRADO EM: {}".format(ativoCesta))
+                print("PREÇO: {}".format(precoLimit))
+                print("HORÁRIO: {}".format(horario))
                 print('----------------------------------')
+
+                file.close()
 
         else:
             for operacao in operacoesAbertas:
@@ -139,18 +169,19 @@ def obterSinal(minima, fechamento):
                         symbol=operacao)
                     tick = cliente.futures_symbol_ticker(symbol=operacao)
 
-                    precoAtual = float(tick['price'])
                     quantidade = infoPosicao[0]['positionAmt']
 
-                    if precoAtual > mediaMovel[-1]:
-                        fecharPosicao(ativo=operacao,
-                                      lote=quantidade, lado=SIDE_SELL)
+                    if float(fechamento) > mediaMovelTresUltimasMaximas[-1]:
+                        # fecharPosicao(ativo=operacao,
+                        #               lote=quantidade, lado=SIDE_SELL)
                         operacoesAbertas.remove(operacao)
                         print('---------------------------------------------')
                         print("OPERAÇÃO FINALIZADA EM: {}".format(operacao))
+                        print("PREÇO: {}".format(float(fechamento)))
+                        print("HORÁRIO: {}".format(horario))
                         print('---------------------------------------------')
 
-            if rsi[-1] <= 5.0 and float(centavos[1]) == 0.0:
+            if condicaoCompra(fechamento=fechamento, ma21Fechamento=mediaMovelVinteUmFechamentos, ma3Minimas=mediaMovelTresUltimasMinimas, centavos=centavos):
                 infoPosicao = cliente.futures_position_information(
                     symbol=ativoCesta)
 
@@ -160,8 +191,8 @@ def obterSinal(minima, fechamento):
                     print("Condição de compra acionada, mas já existe uma posição aberta em {}".format(
                         ativoCesta))
                 else:
-                    abrirPosicao(ativo=ativoCesta, lote=0.5,
-                                 lado=SIDE_BUY, preco=precoLimit)
+                    # abrirPosicao(ativo=ativoCesta, lote=0.5,
+                    #              lado=SIDE_BUY, preco=precoLimit)
                     print('----------------------------------')
                     print("COMPRADO EM: {}".format(ativoCesta))
                     print('----------------------------------')
