a216ebc2ea2a3e9b077e530b7693c4b5d8c24c9f
thiagomunich <thiagomunich@gmail.com>
2021-09-03 15:27:56 -0300

    inserção de saida parcial

--- Main.py
+++ Main.py
@@ -15,11 +15,15 @@ from Negociacao import abrirPosicao, condicaoAbrirCompra, condicaoFecharCompra, 
 from Telegram import mensagemTelegram
 
 # SOCKET = "wss://fstream.binance.com/ws/btcusdt@kline_1m"
-SOCKET = "wss://stream.binance.com:9443/ws/btcusdt@kline_5m"
+SOCKET = "wss://stream.binance.com:9443/ws/btcusdt@kline_1m"
 
-precoEntrada = 0.0
-resultadoAcumulado = 0.0
+demaLow = 0.0
 stopLoss = 0.0
+demaHigh = 0.0
+precoEntrada = 0.0
+parcialVenda = False
+parcialCompra = False
+resultadoAcumulado = 0.0
 
 agora = datetime.datetime.utcnow()
 teste = datetime.datetime.strftime(agora, '%Y/%m/%d %H:%M:00')
@@ -34,10 +38,11 @@ def onClose(ws):
 
 
 def onMessage(ws, mensagem):
-    global resultadoAcumulado
+    global stopLoss
+    global parcialVenda
+    global parcialCompra
     global precoEntrada
-
-    horarioFormatado = datetime.datetime.strftime(agora, '%d/%m/%Y %H:%M:00')
+    global resultadoAcumulado
 
     mensagemJson = json.loads(mensagem)
 
@@ -45,11 +50,17 @@ def onMessage(ws, mensagem):
 
     candleFechado = candle['x']
 
+    agora = datetime.datetime.utcnow()
+
+    horarioFormatado = datetime.datetime.strftime(agora, '%d/%m/%Y %H:%M:00')
+
     if candleFechado:
 
         obterSinal()
     else:
         tick = float(candle['c'])
+
+        demaHigh, demaLow, = montarDataframe(esperarFechamento=False)
 
         if operacoesAbertas[0] == 'comprado':
             if tick < stopLoss:
@@ -66,6 +77,25 @@ def onMessage(ws, mensagem):
 
                 operacoesAbertas.pop()
 
+            elif tick >= demaHigh and tick > precoEntrada and parcialCompra == False:
+
+                resultadoAtual = (tick - precoEntrada) / 2
+
+                resultadoAcumulado += (tick - precoEntrada) / 2
+
+                stopLoss = precoEntrada
+
+                parcialCompra = True
+
+                print('Parcial de compra acionada.')
+
+                print('Novo SL: {}'.format(stopLoss))
+
+                msg = 'PARCIAL COMPRA\n\nHorário saída: {}\n\nResultado da operação: {} USD\n\nResultado acumulado: {} USD'.format(
+                    horarioFormatado, round(resultadoAtual, 2), round(resultadoAcumulado, 2))
+
+                mensagemTelegram(mensagem=msg)
+
         elif operacoesAbertas[0] == 'vendido':
             if tick > stopLoss:
                 print('Stop loss de venda acionado.')
@@ -80,6 +110,24 @@ def onMessage(ws, mensagem):
                 mensagemTelegram(mensagem=msg)
 
                 operacoesAbertas.pop()
+
+            elif tick <= demaLow and tick < precoEntrada and parcialVenda == False:
+
+                resultadoAtual = (precoEntrada - tick) / 2
+
+                resultadoAcumulado += (precoEntrada - tick) / 2
+
+                stopLoss = precoEntrada
+
+                parcialVenda = True
+
+                print('Parcial de venda acionada.')
+                print('Novo SL: {}'.format(stopLoss))
+
+                msg = 'PARCIAL VENDA\n\nHorário saída: {}\n\nResultado da operação: {} USD\n\nResultado acumulado: {} USD'.format(
+                    horarioFormatado, round(resultadoAtual, 2), round(resultadoAcumulado, 2))
+
+                mensagemTelegram(mensagem=msg)
 
 
 def mensagemSaidaOperacao(preco, atual, lado):
@@ -126,20 +174,9 @@ def binanceDataFrame(self, klines):
     return df
 
 
-def obterSinal():
-    global stopLoss
-    global precoEntrada
-    global resultadoAcumulado
-
-    # pra dar tempo de pegar os dados do candle que tá aberto, e depois removê-lo
-    # mantendo assim somente até o ultimo candle fechado
-    time.sleep(3)
-
-    agora = datetime.datetime.utcnow()
-    horarioFormatado = datetime.datetime.strftime(agora, '%d/%m/%Y %H:%M:00')
-
+def montarDataframe(esperarFechamento):
     dados = np.array(cliente.get_klines(
-        symbol='BTCUSDT', interval=KLINE_INTERVAL_5MINUTE))
+        symbol='BTCUSDT', interval=KLINE_INTERVAL_1MINUTE))
 
     df = binanceDataFrame(dados, dados)
 
@@ -147,7 +184,8 @@ def obterSinal():
 
     df = df[['Open', 'High', 'Low', 'Close', 'Volume']]
 
-    df = df[:-1]
+    if(esperarFechamento):
+        df = df[:-1]
 
     high, low, close = df['High'], df['Low'], df['Close']
 
@@ -157,58 +195,90 @@ def obterSinal():
 
     atr = talib.ATR(high, low, close, 11)
 
-    differeceBetweenCloseAndLow = close[-1] - low[-1]
-    differeceBetweenCloseAndHigh = high[-1] - close[-1]
+    if(esperarFechamento):
+        return high[-1], low[-1], close[-1], demaHigh[-1], demaLow[-1], atr[-1]
+    else:
+        return demaHigh[-1], demaLow[-1]
 
-    centavosLow = float(str(low[-1]).split('.')[1])
-    centavosHigh = float(str(high[-1]).split('.')[1])
+
+def obterSinal():
+    global stopLoss
+    global precoEntrada
+    global parcialVenda
+    global parcialCompra
+    global resultadoAcumulado
+
+    # pra dar tempo de pegar os dados do candle que tá aberto, e depois removê-lo
+    # mantendo assim somente até o ultimo candle fechado
+    time.sleep(3)
+
+    agora = datetime.datetime.utcnow()
+    horarioFormatado = datetime.datetime.strftime(agora, '%d/%m/%Y %H:%M:00')
+
+    high, low, close, demaHigh, demaLow, atr = montarDataframe(
+        esperarFechamento=True)
+
+    differeceBetweenCloseAndLow = close - low
+    differeceBetweenCloseAndHigh = high - close
+
+    centavosLow = float(str(low).split('.')[1])
+    centavosHigh = float(str(high).split('.')[1])
 
     # print('Fechamento: {} | DemaHigh: {} | DemaLow: {} | Horário: {}'.format(
-    #     close[-1], demaHigh[-1], demaLow[-1], horario))
+    #     close, demaHigh, demaLow, horario))
 
     if len(operacoesAbertas) == 0:
         print('Aguardando sinal...')
-        if close[-1] < demaLow[-1] and centavosLow == 0 and differeceBetweenCloseAndLow < 100:
+        # if close < demaLow and centavosLow == 0 and differeceBetweenCloseAndLow < 200:
+        if close < demaLow:
             # abrirPosicao(ativo=ativoCesta, lote=0.5,
             #              lado=SIDE_BUY, preco=precoLimit)
 
             # remover quando for operar em conta real
             operacoesAbertas.append('comprado')
-            precoEntrada = close[-1]
-            stopLoss = low[-1] - atr[-1] * 0.1
+            precoEntrada = close
+            stopLoss = low - atr
 
             mensagem = 'COMPRADO\n\nHorário entrada: {}'.format(
                 horarioFormatado)
 
             mensagemTelegram(mensagem=mensagem)
 
-            mensagemEntradaOperacao(preco=close[-1], lado='COMPRA')
+            mensagemEntradaOperacao(preco=close, lado='COMPRA')
 
-        elif close[-1] > demaHigh[-1] and centavosHigh == 0 and differeceBetweenCloseAndHigh < 100:
+        # elif close > demaHigh and centavosHigh == 0 and differeceBetweenCloseAndHigh < 200:
+
+        elif close > demaHigh:
             # abrirPosicao(ativo=ativoCesta, lote=0.5,
             #              lado=SIDE_BUY, preco=precoLimit)
 
             # remover quando for operar em conta real
             operacoesAbertas.append('vendido')
-            precoEntrada = close[-1]
-            stopLoss = high[-1] + atr[-1] * 0.1
+            precoEntrada = close
+            stopLoss = high + atr
 
             mensagem = 'VENDIDO\n\nHorário entrada: {}'.format(
                 horarioFormatado)
 
             mensagemTelegram(mensagem=mensagem)
 
-            mensagemEntradaOperacao(preco=close[-1], lado='VENDA')
+            mensagemEntradaOperacao(preco=close, lado='VENDA')
 
     else:
         if operacoesAbertas[0] == 'comprado':
             print('Posição de compra ainda aberta.')
-            if close[-1] > demaHigh[-1]:
+            if close > demaHigh:
 
                 operacoesAbertas.pop()
 
-                resultadoAtual = close[-1] - precoEntrada
-                resultadoAcumulado += close[-1] - precoEntrada
+                if parcialCompra:
+                    resultadoAtual = (close - precoEntrada) / 2
+                    resultadoAcumulado += (close - precoEntrada) / 2
+                else:
+                    resultadoAtual = close - precoEntrada
+                    resultadoAcumulado += close - precoEntrada
+
+                parcialCompra = False
 
                 mensagem = 'COMPRA FECHADA\n\nHorário saída: {}\n\nResultado da operação: {} USD\n\nResultado acumulado: {} USD'.format(
                     horarioFormatado, round(resultadoAtual, 2), round(resultadoAcumulado, 2))
@@ -216,16 +286,25 @@ def obterSinal():
                 mensagemTelegram(mensagem=mensagem)
 
                 mensagemSaidaOperacao(
-                    preco=close[-1], atual=resultadoAtual, lado='COMPRA')
+                    preco=close, atual=resultadoAtual, lado='COMPRA')
 
         elif operacoesAbertas[0] == 'vendido':
             print('Posição de venda ainda aberta.')
-            if close[-1] < demaLow[-1]:
+            if close < demaLow:
 
                 operacoesAbertas.pop()
 
-                resultadoAtual = precoEntrada - close[-1]
-                resultadoAcumulado += precoEntrada - close[-1]
+                if parcialVenda:
+                    resultadoAtual = (precoEntrada - close) / 2
+                    resultadoAcumulado += (precoEntrada - close) / 2
+                else:
+                    resultadoAtual = precoEntrada - close
+                    resultadoAcumulado += precoEntrada - close
+
+                parcialVenda = False
+
+                resultadoAtual = precoEntrada - close
+                resultadoAcumulado += precoEntrada - close
 
                 mensagem = 'VENDA FECHADA\n\nHorário saída: {}\n\nResultado da operação: {} USD\n\nResultado acumulado: {} USD'.format(
                     horarioFormatado, round(resultadoAtual, 2), round(resultadoAcumulado, 2))
@@ -233,7 +312,7 @@ def obterSinal():
                 mensagemTelegram(mensagem=mensagem)
 
                 mensagemSaidaOperacao(
-                    preco=close[-1], atual=resultadoAtual, lado='VENDA')
+                    preco=close, atual=resultadoAtual, lado='VENDA')
 
 
 ws = websocket.WebSocketApp(SOCKET, on_open=onOpen,
